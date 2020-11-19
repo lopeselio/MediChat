a641367a9f3d8c3a45f58d01bd35d01cb53ebf39
lopeselio <lopeselio@gmail.com>
2020-11-19 15:43:02 +0530

    withRouter

--- src/index.js
+++ src/index.js
@@ -5,18 +5,31 @@ import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
+import firebase from './firebase'
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css'
-const Root = () => (
-  <Router>
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.props.history.push('/')
+      }
+    })
+  }
+  render(){
+    return(
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
     </Switch>
-  </Router>
-);
+    )
+  }
+}
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+const RootWithAuth = withRouter(Root)
+
+ReactDOM.render(
+  <Router><RootWithAuth /></Router>, document.getElementById("root"));
 registerServiceWorker();
